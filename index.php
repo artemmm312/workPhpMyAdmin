30c410e2b6949d976d212b53cd79c5c3c380c72d
artemmm312 <artemmm312@gmail.com>
2022-01-18 15:45:19 +0300

    запросы

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ class Connection
 
 $db = Connection::getInstance();
 
+//Вывод заказов
 $result = $db->pdo
 	->query("SELECT `order`.`id` as `id заказа`, 
 	`order`.`date` as `Дата заказа`, 
@@ -55,9 +56,10 @@ $result = $db->pdo
 	FROM `order` 
 	JOIN shoppingcart ON shoppingcart.id = `order`.shopcartID 
 	JOIN user ON shoppingcart.userID = user.id 
-	JOIN product ON shoppingcart.productID = product.id;")
+	JOIN product ON shoppingcart.productID = product.id")
 	->fetchAll();
 
+echo "Все заказы:<br>";
 echo "<table border='1' cellspacing='0' width='50%'>";
 echo "<tr>";
 foreach ($result[0] as $key => $value)
@@ -75,3 +77,100 @@ for ($i = 0; $i < count($result); $i++)
 	echo "</tr>";
 }
 echo "</table>";
+
+echo "<br>";
+
+//Вывод заказов и корзин
+$result2 = $db->pdo
+	->query("SELECT `order`.`id` as `id заказа`, 
+	`order`.`date` as `Дата заказа`, 
+	user.firstName as `Имя`, 
+	user.lastName as `Фамилия`, 
+	product.id as `id продукта`, 
+	product.name as `Наименование продукта`, 
+	product.price as `Цена`
+	FROM `user` 
+	LEFT JOIN shoppingcart ON shoppingcart.userID = user.id
+	LEFT JOIN `order` ON shoppingcart.id = `order`.shopcartID
+	LEFT JOIN product ON shoppingcart.productID = product.id")
+	->fetchAll();
+
+echo "Все заказы, пользователи и корзины:<br>";
+echo "<table border='1' cellspacing='0' width='50%'>";
+echo "<tr>";
+foreach ($result2[0] as $key => $value)
+{
+	echo "<th>$key</th>";
+}
+echo "</tr>";
+for ($i = 0; $i < count($result2); $i++)
+{
+	echo "<tr>";
+	foreach ($result2[$i] as $key => $value)
+	{
+		echo "<td>$value</td>";
+	}
+	echo "</tr>";
+}
+echo "</table>";
+
+echo "<br>";
+
+//Вывод пользователей и их заказов
+$result3 = $db->pdo
+	->query("SELECT `order`.`id` as `id заказа`, 
+	`order`.`date` as `Дата заказа`, 
+	user.firstName as `Имя`, 
+	user.lastName as `Фамилия`, 
+	product.id as `id продукта`, 
+	product.name as `Наименование продукта`, 
+	product.price as `Цена`
+	FROM `order` 
+	LEFT JOIN shoppingcart ON shoppingcart.id = `order`.shopcartID 
+	RIGHT JOIN user ON shoppingcart.userID = user.id 
+	LEFT JOIN product ON shoppingcart.productID = product.id")
+	->fetchAll();
+
+echo "Все пользователи и сделанные ими заказы:<br>";
+echo "<table border='1' cellspacing='0' width='50%'>";
+echo "<tr>";
+foreach ($result3[0] as $key => $value)
+{
+	echo "<th>$key</th>";
+}
+echo "</tr>";
+for ($i = 0; $i < count($result3); $i++)
+{
+	echo "<tr>";
+	foreach ($result3[$i] as $key => $value)
+	{
+		echo "<td>$value</td>";
+	}
+	echo "</tr>";
+}
+echo "</table>";
+echo "<br>";
+
+//Топ 5 самых дорогих товаров
+$result4 = $db->pdo
+	->query("SELECT * FROM product ORDER BY price DESC LIMIT 5")
+	->fetchAll();
+
+echo "<table border='1' cellspacing='0' width='50%'>";
+echo "<tr>";
+foreach ($result4[0] as $key => $value)
+{
+	echo "<th>$key</th>";
+}
+echo "</tr>";
+for ($i = 0; $i < count($result4); $i++)
+{
+	echo "<tr>";
+	foreach ($result4[$i] as $key => $value)
+	{
+		echo "<td>$value</td>";
+	}
+	echo "</tr>";
+}
+echo "</table>";
+echo "<br>";
